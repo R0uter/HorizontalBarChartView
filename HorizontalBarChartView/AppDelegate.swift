9fc010bc11e9f8a0f2dd4dca21eb01294e6af370
R0uter <lomyr.jo@gmail.com>
2017-12-18 21:20:30 +0800

    Initial Commit

---
+++ HorizontalBarChartView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HorizontalBarChartView
+//
+//  Created by R0uter on 12/18/17.
+//  Copyright © 2017 logcg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HorizontalBarChartView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HorizontalBarChartView
+//
+//  Created by R0uter on 12/18/17.
+//  Copyright © 2017 logcg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
